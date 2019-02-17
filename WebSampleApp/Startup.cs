a4420891631c9b7da85391454a0fa8269332067d
Rostyslav <RokaRostuk@gmail.com>
2019-02-17 15:57:53 +0200

    Added static files

--- WebSampleApp/Startup.cs
+++ WebSampleApp/Startup.cs
@@ -26,6 +26,8 @@ namespace WebSampleApp
                 app.UseDeveloperExceptionPage();
             }
 
+            app.UseStaticFiles();
+
             app.Run(async (context) =>
             {
                 // await context.Response.WriteAsync("Hello World!");
